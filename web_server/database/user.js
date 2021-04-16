45fc6a75de24b98041888a6d75544736988428c0
Tenzin Wangpo <wangpo7298@gmail.com>
2021-04-16 13:13:20 -0700

    all branch are merge to authentication branch, now copying authentication branch to master branch

---
+++ web_server/database/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const findOrCreate = require('mongoose-findorcreate');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        // required: true
+    },
+    password: {
+        type: String,
+        // required: true
+    },
+    googleId: {
+        type: String
+    }
+
+});
+
+UserSchema.plugin(findOrCreate);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
