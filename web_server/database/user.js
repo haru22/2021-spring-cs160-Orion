135c8b878279e47b74d58d3bf370e6e29d344691
Tenzin Wangpo <wangpo7298@gmail.com>
2021-04-21 18:59:25 -0700

    Small changes on Oauth2.0

--- web_server/database/user.js
+++ web_server/database/user.js
@@ -12,6 +12,9 @@ const UserSchema = new mongoose.Schema({
         type: String,
         // required: true
     },
+    username: {
+        type: String,
+    },
     googleId: {
         type: String
     }
